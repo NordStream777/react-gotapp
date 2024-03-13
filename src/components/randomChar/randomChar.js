801361f254d28ed5cc35e5e18d34d45fb6705c9e
NordStream777 <NRDStream@gmail.com>
2024-03-13 08:39:56 +0300

    add pages

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -1,54 +1,53 @@
 import React, {Component} from 'react';
 import './randomChar.css';
 import gotService from '../../services/gotService';
-import Spinner from '../spinner/';
+import Spinner from '../spinner';
 import ErrorMessage from '../error/errorMessage';
-
-
 export default class RandomChar extends Component {
+
     gotService = new gotService();
     state = {
-        char : {},
+        char: {},
         loading: true,
         error: false
-    };
+    }
 
-    componentDidMount(){
+    componentDidMount() {
         this.updateChar();
+        this.timerId = setInterval(this.updateChar, 15000);
     }
 
     componentWillUnmount(){
+        clearInterval(this.timerId);
     }
 
     onCharLoaded = (char) => {
         this.setState({
             char,
             loading: false
-        });
-    };
+        })
+    }
 
     onError = (err) => {
         this.setState({
             error: true,
             loading: false
-        });
+        })
     }
 
-    updateChar() {
-        const id = Math.floor(Math.random()*140 + 25);
-        this.gotService
-            .getCharacter(id)
+    updateChar = () => {
+        const id = Math.floor(Math.random()*140 + 25); //25-140
+        this.gotService.getCharacter(id)
             .then(this.onCharLoaded)
             .catch(this.onError);
     }
 
     render() {
+        const { char, loading, error } = this.state;
 
-        const {char, loading, error} = this.state;
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
-        const content = !(loading || error) ? <View char={char} /> : null;
-        
+        const content = !(loading || error) ? <View char={char}/> : null;
 
         return (
             <div className="random-block rounded">
@@ -59,10 +58,8 @@ export default class RandomChar extends Component {
         );
     }
 }
-
 const View = ({char}) => {
     const {name, gender, born, died, culture} = char;
-
     return (
         <>
             <h4>Random Character: {name}</h4>
