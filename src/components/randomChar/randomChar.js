17df89de307911b548638421d94070a0c08718fe
NordStream777 <NRDStream@gmail.com>
2024-03-21 07:40:12 +0300

    finish

--- src/components/randomChar/randomChar.js
+++ src/components/randomChar/randomChar.js
@@ -12,9 +12,13 @@ export default class RandomChar extends Component {
         error: false
     }
 
+    static defaultProps = {
+        interval : 15000
+    }
+
     componentDidMount() {
         this.updateChar();
-        this.timerId = setInterval(this.updateChar, 15000);
+        this.timerId = setInterval(this.updateChar, this.props.interval);
     }
 
     componentWillUnmount(){
@@ -58,6 +62,11 @@ export default class RandomChar extends Component {
         );
     }
 }
+
+RandomChar.defaultProps = {
+    interval : 15000
+}
+
 const View = ({char}) => {
     const {name, gender, born, died, culture} = char;
     return (
